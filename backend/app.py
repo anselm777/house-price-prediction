0caeaf20f2789bdab40e14736c4ea7b7422a8073
anselm777 <anselmonteiro62@gmail.com>
2026-05-04 19:06:22 +0530

    fix render port binding

--- backend/app.py
+++ backend/app.py
@@ -29,5 +29,9 @@ def predict():
 
     return jsonify({'price': prediction[0]})
 
+
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
